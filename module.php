f1e90ea21346caa4239eb485130b86da27665a22
Nils Asmussen <nils@script-solution.de>
2008-07-27 19:43:28 +0000

    Added a few convenience-methods to the module

--- module.php
+++ module.php
@@ -44,6 +44,35 @@ abstract class PLIB_Module extends PLIB_Object
 	public abstract function run();
 	
 	/**
+	 * Creates the formular, adds it to the template and allows all methods of it
+	 * to be called.
+	 *
+	 * @return PLIB_HTML_Formular the created formular
+	 */
+	protected function request_formular()
+	{
+		$tpl = PLIB_Props::get()->tpl();
+
+		$form = new PLIB_HTML_Formular(false,false);
+		$tpl->add_array('form',$form);
+		$tpl->add_allowed_method('form','*');
+		return $form;
+	}
+	
+	/**
+	 * Reports an error and stores that the module has not finished in a correct way.
+	 * Note that you have to specify a message if the type is no error and no no-access-msg!
+	 *
+	 * @param int $type the type. see PLIB_Messages::MSG_TYPE_*
+	 * @param string $message you can specify the message to display here, if you like
+	 */
+	protected function report_error($type = PLIB_Messages::MSG_TYPE_ERROR,$message = '')
+	{
+		$doc = PLIB_Props::get()->doc();
+		$doc->report_error($type,$message);
+	}
+	
+	/**
 	 * @see PLIB_Object::get_print_vars()
 	 *
 	 * @return array
