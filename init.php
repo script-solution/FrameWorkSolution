ef3b7373d0a308a581a8463158366d3eb6330249
Nils Asmussen <nils@script-solution.de>
2013-01-13 23:54:43 +0100

    Changed version to 1.43

--- init.php
+++ init.php
@@ -32,12 +32,12 @@ if(version_compare(PHP_VERSION,'5.2.0','<'))
 /**
  * The version of the framework
  */
-define('FWS_VERSION','FrameWorkSolution v1.42');
+define('FWS_VERSION','FrameWorkSolution v1.43');
 
 /**
  * The version-id of frameworksolution
  */
-define('FWS_VERSION_ID','142');
+define('FWS_VERSION_ID','143');
 
 // include the path-object. we need this for the autoload-function
 include_once(FWS_PATH.'utilbase.php');
