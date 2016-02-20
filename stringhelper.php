cc161b50e8a9a81aae984b710c83cebd70a64a6c
Nils Asmussen <nils@script-solution.de>
2016-02-20 21:37:33 +0100

    Fixed return type (by PHPCheck).

--- stringhelper.php
+++ stringhelper.php
@@ -193,7 +193,7 @@ final class FWS_StringHelper extends FWS_UtilBase
 		$p = '/^[a-z0-9!#$%&*+-=?^_`{|}~]+(\.[a-z0-9!#$%&*+-=?^_`{|}~]+)*';
 		$p.= '@([-a-z0-9]+\.)+([a-z]{2,3}';
 		$p.= '|info|arpa|aero|coop|name|museum)$/ix';
-		return preg_match($p,$mail);
+		return preg_match($p,$mail) === 1;
 	}
 	
 	/**
